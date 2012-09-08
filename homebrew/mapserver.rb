747d05da603002b297a7d6dcad21f66367a44ef2
Thomas Bonfort <thomas.bonfort@gmail.com>
2012-09-08 15:14:13 +0200

    add python mapscript support, swig dependency

--- homebrew/mapserver.rb
+++ homebrew/mapserver.rb
@@ -1,5 +1,13 @@
 require 'formula'
 
+def no_python?
+  ARGV.include? "--without-python"
+end
+
+def which_python
+  "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
+end
+
 class Mapserver < Formula
   homepage 'http://mapserver.org/'
   url 'http://download.osgeo.org/mapserver/mapserver-6.2.0-beta3.tar.gz'
@@ -16,6 +24,7 @@ class Mapserver < Formula
   depends_on 'fcgi' unless ARGV.include? '--without-fastcgi'
   depends_on 'giflib' if ARGV.include? '--with-gif'
   depends_on 'cairo' if ARGV.include? '--with-cairo'
+  depends_on 'swig' if ARGV.build_head?
 
   #postgis support enabled by default
   depends_on 'postgresql' if not MacOS.lion? and not ARGV.include? '--without-postgresql'
@@ -30,7 +39,8 @@ class Mapserver < Formula
       ["--with-cairo", "Enable support for cairo SVG and PDF output"],
       ["--without-postgresql", "Disable support for PostgreSQL as a data source"],
       ["--without-gdal", "Disable support for GDAL raster access"],
-      ["--without-fastcgi", "Disable FastCGI support"]
+      ["--without-fastcgi", "Disable FastCGI support"],
+      ['--without-python', 'Disable Python mapscript support'],
     ]
   end
 
@@ -51,6 +61,7 @@ class Mapserver < Formula
     args.push "--with-cairo" if ARGV.include? '--with-cairo'
     args.push "--with-php" if ARGV.include? '--with-php'
     args.push "--with-fastcgi" unless ARGV.include? '--without-fastcgi'
+    args.push "--enable-python-mapscript" unless ARGV.include? '--without-python'
 
     unless ARGV.include? '--without-postgresql'
       if MacOS.lion? # Lion ships with PostgreSQL libs
@@ -68,15 +79,20 @@ class Mapserver < Formula
 
   def install
     ENV.x11
+    python_lib = lib + which_python + 'site-packages'
     system "./configure", *configure_args
     system "make"
-    system "make PHP_EXT_DIR=#{prefix}/lib"
+    system "make PHP_EXT_DIR=#{prefix}/lib PYLIBDIR=#{python_lib}" 
     system "make install-bin"
          
 
     if ARGV.include? '--with-php'
       system "make PHP_EXT_DIR=#{prefix}/lib php_mapscript_install"
     end
+    unless no_python?
+      system "make PYLIBDIR=#{python_lib} python_mapscript_install" 
+    end
+
   end
 
   def caveats
@@ -84,6 +100,7 @@ class Mapserver < Formula
 The Mapserver CGI executable is /usr/local/bin/mapserv
 You can add it to your system apache webserver with the command:
 sudo ln -s /usr/local/bin/mapserv /Library/WebServer/CGI-Executables/mapserv
+
     EOS
     if ARGV.include? '--with-php'
       s << <<-EOS
@@ -92,6 +109,23 @@ To activate PHP mapscript:
     extension="/usr/local/lib/php_mapscript.so"
   * Execute "php -m"
   * You should see MapScript in the module list
+
+      EOS
+    end
+    unless no_python?
+      if ARGV.build_head?
+      s << <<-EOS
+Swig versions > 2.0.4 fail to build working mapscript bindings. Either install
+a release version of mapserver, or make sure your swig is installed/downgraded
+to at most 2.0.4 
+      EOS
+      end
+      s << <<-EOS
+Unless you are using Homebrew's Python, the python mapscript bindings
+will be unusable unless the following directory is added to the PYTHONPATH:
+
+    #{HOMEBREW_PREFIX}/lib/#{which_python}/site-packages
+
       EOS
     end
     return s
